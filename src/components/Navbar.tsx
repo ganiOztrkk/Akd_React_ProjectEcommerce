1e28350d26cdefbea90a662acc94bc8b451e030b
Gani Ozturk <127308866+ganiOztrkk@users.noreply.github.com>
2023-09-06 23:19:51 +0300

    formatted

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,71 +1,99 @@
-import React, { useEffect, useState } from 'react'
-import { IUser } from '../models/IUser'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { decrypt } from '../util'
+import React, { useEffect, useState } from "react";
+import { IUser } from "../models/IUser";
+import { NavLink, useNavigate } from "react-router-dom";
+import { decrypt } from "../util";
 
 function Navbar() {
+  const [item, setItem] = useState<IUser>();
+  const navigate = useNavigate();
+  useEffect(() => {
+    var stData = localStorage.getItem("user");
+    if (stData !== null) {
+      try {
+        stData = decrypt(stData);
+        const jsonObj = JSON.parse(stData) as IUser;
+        setItem(jsonObj);
+      } catch (error) {
+        localStorage.removeItem("user");
+        navigate("/");
+      }
+    } else {
+      navigate("/");
+    }
+  }, []);
 
-  const [item, setItem] = useState<IUser>()
-    const navigate = useNavigate()
-    useEffect( () => {
-    var stData = localStorage.getItem("user")
-        if(stData !== null){
-          try {
-            stData = decrypt(stData)
-            const jsonObj = JSON.parse(stData) as IUser
-            setItem(jsonObj)
-          } catch (error) {
-            localStorage.removeItem("user")
-            navigate('/')
-          }
-            
-        }
-        else{
-          navigate('/')
-        }
-    }, [])
-
-const logOut= () =>{
-  const answer = window.confirm("Çıkmak istediğinizden emin misiniz?")
-  if(answer === true){
-    localStorage.removeItem("user")
-    navigate('/')
-  }
-}
+  const logOut = () => {
+    const answer = window.confirm("Çıkmak istediğinizden emin misiniz?");
+    if (answer === true) {
+      localStorage.removeItem("user");
+      navigate("/");
+    }
+  };
 
   return (
     <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">React App</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink to='/dashboard' className="nav-link">Dashboard</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/' className="nav-link">Anasayfa</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/profile' className="nav-link">Profile</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='' className="nav-link disabled">{item?.name}</NavLink>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success me-2" type="submit">Search</button>
-        <button onClick={logOut} className="btn btn-outline-danger" type="submit">Logout</button>
-      </form>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            React App
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="/dashboard" className="nav-link">
+                  Dashboard
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">
+                  Anasayfa
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/profile" className="nav-link">
+                  Profile
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="" className="nav-link disabled">
+                  {item?.name}
+                </NavLink>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success me-2" type="submit">
+                Search
+              </button>
+              <button
+                onClick={logOut}
+                className="btn btn-outline-danger"
+                type="submit"
+              >
+                Logout
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,64 +1,78 @@
-import React, {useState, FormEvent } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { encrypt } from '../util'
+import React, { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { encrypt } from "../util";
 
 function Login() {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState("")
-  const [errorMessage, setErrorMessage] = useState('')
+  //fnc Login
+  const fcnLogin = (evt: FormEvent) => {
+    evt.preventDefault();
+    if (email === "gani@gmail.com" && password === "12345") {
+      //geçici yani tarayıcı acık oldugu sürece yaşayan data - session storage
+      //birinci parametre - key , ikinci parametre - value
+      const item = {
+        email: email,
+        name: "Ali Bilmem",
+        phone: "543 230 2300",
+      };
+      var stItem = JSON.stringify(item);
+      stItem = encrypt(stItem);
 
-//fnc Login
-const fcnLogin = (evt: FormEvent) => {
-  evt.preventDefault()
-  if(email === "gani@gmail.com" && password ==="12345"){
-    //geçici yani tarayıcı acık oldugu sürece yaşayan data - session storage
-    //birinci parametre - key , ikinci parametre - value
-    const item ={
-      email: email,
-      name: "Ali Bilmem",
-      phone: "543 230 2300"
+      localStorage.setItem("user", stItem);
+      navigate("/dashboard");
+      //sessionStorage.setItem("user",email)
+    } else {
+      setErrorMessage("Kullanıcı adı yada şifre hatalı.");
     }
-    var stItem = JSON.stringify(item)
-    stItem = encrypt(stItem)
-    
-    localStorage.setItem("user", stItem)
-    navigate('/dashboard')
-    //sessionStorage.setItem("user",email)
-  }
-  else{
-    setErrorMessage("Kullanıcı adı yada şifre hatalı.")
-  }
-
-}
+  };
 
   return (
-    <div className='row'>
-        <div className='col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-4 col-xxl-4'></div>
+    <div className="row">
+      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-4 col-xxl-4"></div>
 
-        <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4'>
-          <form onSubmit={fcnLogin}>
-            <h2>Admin Login</h2>
-            {errorMessage !== "" &&
-            <div className="alert alert-warning alert-dismissible fade show" role="alert">
+      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4">
+        <form onSubmit={fcnLogin}>
+          <h2>Admin Login</h2>
+          {errorMessage !== "" && (
+            <div
+              className="alert alert-warning alert-dismissible fade show"
+              role="alert"
+            >
               <strong>Dikkat!</strong> {errorMessage}
-              <button type="button" className="btn-close" onClick={() => setErrorMessage('')}></button>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={() => setErrorMessage("")}
+              ></button>
             </div>
-            }
-            <div  className='mb-3 mt-3'>
-              <input onChange={(evt) => setEmail(evt.target.value)} type='email' className='form-control' placeholder='E-Mail'/>
-            </div>
-            <div  className='mb-3 mt-3'>
-              <input onChange={(evt) => setPassword(evt.target.value)} type='password' className='form-control' placeholder='Password'/>
-            </div>
-            <button className='btn btn-primary mt-2 mb-2'>Login</button>
-          </form>    
-        </div>
+          )}
+          <div className="mb-3 mt-3">
+            <input
+              onChange={(evt) => setEmail(evt.target.value)}
+              type="email"
+              className="form-control"
+              placeholder="E-Mail"
+            />
+          </div>
+          <div className="mb-3 mt-3">
+            <input
+              onChange={(evt) => setPassword(evt.target.value)}
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
+          <button className="btn btn-primary mt-2 mb-2">Login</button>
+        </form>
+      </div>
 
-        <div className='col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-4 col-xxl-4'></div>
+      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-4 col-xxl-4"></div>
     </div>
-  )
+  );
 }
-export default Login
+export default Login;
